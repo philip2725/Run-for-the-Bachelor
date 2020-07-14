90077148e178964113de3d8a889d9aed2a82188b
BFWI418ABS <andre.bosbach@edu.fhdw.de>
2020-07-14 17:15:19 +0200

    Movement Animation mit 2 Bildern + Feste Position des Spielers integriert

--- JS/game.js
+++ JS/game.js
@@ -1,6 +1,3 @@
-//************* Ich bin ein Testkommentar <3 ***************//
-
-
 //************* Globale Variablen (Config) ***************//
 //Window
 var canvas, ctx;
@@ -8,15 +5,19 @@ var gameWidth = 1200;
 var gameHeight = 700;
 
 //character
-var charX = 60;
-var charY = 150;
-var charWidth = 120;
-var	charHeight = 120;
+var charX = 120;
+var charY = 130;
+var charWidth = 160;
+var	charHeight = 160;
+
+var charPictureIds = ['char1', 'char2'];
+var currentPictureIdx = 0;
+var movementspeed = 250;
 
 //move Options
-var ground = charY;
+var ground = charY;		//save the null point of the ground
 var jumpHigh = 250;		//high from the ground
-var jumpSpeed = 10;		//lower = faster
+var jumpSpeed = 15;		//lower = faster
 var jumping;			//jumping Intervall ID
 var goingDown = false;		
 
@@ -27,16 +28,16 @@ function init(){
 	canvas.style.border = "1px solid grey";
 	ctx = canvas.getContext("2d");
 
-	player = document.getElementById("char");
 	setInterval(draw, 40);
-
+	setInterval(changePlayerPicture, movementspeed);
 }
 
 function draw(){
 	ctx.clearRect(0,0,gameWidth,gameHeight)
-	drawRect(0,gameHeight*0.8,gameWidth,gameHeight*0.2, "green")   					//floor
-	drawRect(0,0,gameWidth,gameHeight*0.8, "lightblue")	       					//Background
-	ctx.drawImage(player, gameHeight*0.8-charX,gameWidth*0.5-charY, charWidth, charHeight);		//character Image
+	var floor = drawRect(0,gameHeight*0.8,gameWidth,gameHeight*0.2, "green")   					//floor
+	drawRect(0,0,gameWidth,gameHeight*0.8, "lightblue")	       									//Background
+	ctx.drawImage(player, gameWidth*0.5-(charX/2),gameHeight*0.8-charY, charWidth, charHeight);		//character Image
+	ctx.drawImage()
 }
 
 //*************** Functions ******************//
@@ -46,35 +47,47 @@ function drawRect(rx, ry, rw, rh, rstyle = "#0000FF"){
 }
 
 function jump(){
-if(charY < jumpHigh && !goingDown){
-	charY += 6
-}else {
-	goingDown = true;
-	if(charY == ground){
-		goingDown = false;
-		clearInterval(jumping);
-	}else{
-		charY -= 6
+	if(charY < jumpHigh && !goingDown){
+		charY += 6
+	}else {
+		goingDown = true;
+		if(charY == ground){
+			goingDown = false;
+			clearInterval(jumping);
+		}else{
+			charY -= 6
+		}
 	}
 }
 
-console.log('charY: ' + charY + ' ground: ' + ground + ' ' + goingDown);
+function changePlayerPicture(){
+	
+	if(charPictureIds[currentPictureIdx] == charPictureIds[charPictureIds.length - 1]){
+		currentPictureIdx = 0;
+	}else{
+		currentPictureIdx++;
+	}
+	player = document.getElementById(charPictureIds[currentPictureIdx]);
+	console.log(charPictureIds[currentPictureIdx]);
 }
 
-function goLeft(){
+/*function goLeft(){
 	charX += 10
 }
 
 function goRight(){
 	charX -= 10
-}
+}*/
 
 //************** Event Functions *************//
+//Key-Events: Every key has one keyCode. 
+//Find Out KeyCode Here  ->  https://keycode.info
+
 function keyDown(event){
 	switch (event.keyCode) {
 	  case 37:
 	    // Left-Arrow Pressed
-	    goLeft();
+	    //goLeft();
 	    break;
 	  case 38:
 	    // Up-Arrow Pressed
@@ -82,7 +95,7 @@ function keyDown(event){
 	    break;
 	  case 39:
 	    // Right-Arrow Pressed
-	    goRight();
+	    //goRight();
 	    break;
 	  case 40:
 	    // Down-Arrow Pressed
