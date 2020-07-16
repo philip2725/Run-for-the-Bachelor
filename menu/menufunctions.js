a7ebad0a212fa0c22b0608fb049aad8f741682b8
Dustin Boehlke <dustin.boehlke@edu.fhdw.de>
2020-07-16 16:09:44 +0200

    Hauptmenü | Popup Fenster Datenschutz/ Impressum und Steuerung + TestLogo

---
+++ menu/menufunctions.js
@@ -0,0 +1,20 @@
+
+function toggle(txt) {
+    var txt = document.getElementById(txt);
+   
+    if (txt.style.display == "block") {
+        txt.style.display = "none"}
+    else {
+        txt.style.display = "block"
+    }
+}
+
+function toggleimprs(txt) {
+    var txt = document.getElementById(txt);
+   
+    if (txt.style.display == "block") {
+        txt.style.display = "none"}
+    else {
+        txt.style.display = "block"
+    }
+}
