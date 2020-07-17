141ed04e24d31208b025a979461d9e5c47966e7d
AlexKaluza <alexander.kaluza@edu.fhdw.de>
2020-07-17 12:53:17 +0200

    Hauptmenü | html und css Kommentare hinzugefügt

--- menu/menufunctions.js
+++ menu/menufunctions.js
@@ -1,15 +1,6 @@
 
+// function to change the visibility of an element
 function toggle(txt) {
-    var txt = document.getElementById(txt);
-   
-    if (txt.style.display == "block") {
-        txt.style.display = "none"}
-    else {
-        txt.style.display = "block"
-    }
-}
-
-function toggleimprs(txt) {
     var txt = document.getElementById(txt);
    
     if (txt.style.display == "block") {
