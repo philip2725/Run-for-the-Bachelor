311baf59c98c2df5f5b6b2f6109647ee0c92fcbc
Dustin Boehlke <dustin.boehlke@edu.fhdw.de>
2020-07-21 12:26:02 +0200

    Hauptmenü | Charakterauswahl hinzugefügt (Boy/Girl)

--- menu/menufunctions.js
+++ menu/menufunctions.js
@@ -14,3 +14,24 @@ function getPlayerName() {
     var input = document.getElementById("userInput").value;
     document.getElementById("test").innerHTML = "Hallo " + input;
 }
+
+
+//save the character selection in the sessionStorage
+function saveChosenCharacter(x)
+{ 
+    //0 = Boy   1 = Girl
+    if (x == 0) {
+        sessionStorage.setItem("chosenCharacter", 0) //Boy
+        sessionStorage.setItem("chosenCharGraphic", "../assets/Images/menu/boy.gif")}
+    else {
+        sessionStorage.setItem("chosenCharacter", 1) //Girl
+        sessionStorage.setItem("chosenCharGraphic", "../assets/Images/menu/girl.gif")
+    }
+}
+
+//Change the graphic of the choosen character 
+function getChosenCharGraphic()
+{
+    var img = document.getElementById("showChosenCharacter");
+    img.src = sessionStorage.getItem("chosenCharGraphic");
+}
