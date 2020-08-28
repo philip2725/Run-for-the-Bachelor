9bc9c185fb2341d10145d49ab8a4bc54fa9f3108
Dustin Boehlke <dustin.boehlke@edu.fhdw.de>
2020-08-28 09:37:58 +0200

    Hauptmenü | Charakterauswahl angepasst und Gifs hinzugefügt

--- menu/menufunctions.js
+++ menu/menufunctions.js
@@ -15,21 +15,23 @@ function toggle(txt) {
 }
 
 //  saves the character selection in the sessionStorage
-var gender = 0;
-var y = 0;
+var gender = 0; //0 = Male; 1 = Female
+var charID = 0; //defines the character 
 
 function setGender(int){
     var img = document.getElementById("chosenCharacterGraphic");
     if(int == 0){
         gender = 0; //Male
-        img.src="../assets/Images/Character/Boy/Gifs/Boy_Idle.gif";
+        img.src="../assets/Images/Character/Player/Boys/Boy 1/Gifs/Boy1 Idle R.gif";
+        img.setAttribute("style","height: 344px; width: 228px; margin-left: 300px; margin-top: 18px");
         sessionStorage.setItem("chosenCharacter", 1); //save as Boy 1
-        y = 1;
+        charID = 1;
     }else if (int == 1){
         gender = 1; //Female
-        img.src="../assets/Images/Character/Girl/Gifs/Girl_Idle_Left.gif";
+        img.src="../assets/Images/Character/Player/Girls/Girl 1/Gifs/Girl1 Idle R.gif";
+        img.setAttribute("style","height: 358px; width: 223px; margin-left: 310px;");
         sessionStorage.setItem("chosenCharacter", 4); //save as Girl 1
-        y = 4;
+        charID = 4;
     }
 }
 
@@ -39,64 +41,71 @@ function saveChosenCharacter(x)
     var img = document.getElementById("chosenCharacterGraphic");
     if(gender == 0){
         if(x == 1){
-           if (y >= 0 && y <= 2){
-                y = y + x;
+           if (charID >= 0 && charID <= 2){
+                charID = charID + x;
             }else {
-                y = 1;
+                charID = 1;
             }
         }else if(x == -1){
-            if (y >= 2 && y <= 3){
-                y = y + x;
+            if (charID >= 2 && charID <= 3){
+                charID = charID + x;
             }else{
-                y = 3;
+                charID = 3;
             }
         }  
     }else if(gender == 1){
         if(x == 1){
-            if (y >= 3 && y <= 5){
-                 y = y + x;
+            if (charID >= 3 && charID <= 5){
+                charID = charID + x;
              }else {
-                 y = 4;
+                charID = 4;
              }
          }else if(x == -1){
-             if (y >= 5 && y <= 6){
-                 y = y + x;
+             if (charID >= 5 && charID <= 6){
+                charID = charID + x;
              }else{
-                 y = 6;
+                charID = 6;
              }
          }  
     }    
     
     //  sets Character Graphics and saves the chosen Character in the Session Storage (1-3 -> Boys; 4-6 Girls;)
-    switch (y) {
+    switch (charID) {
         case 1: 
         sessionStorage.setItem("chosenCharacter", 1) //Boy 1
-        img.src ="../assets/Images/Character/Boy/Gifs/Boy_Idle.gif";
+        img.src ="../assets/Images/Character/Player/Boys/Boy 1/Gifs/Boy1 Idle R.gif";
+        img.setAttribute("style","height: 344px; width: 228px; margin-left: 300px; margin-top: 20px");
         break;
     
         case 2:
         sessionStorage.setItem("chosenCharacter", 2) //Boy 2
-        img.src ="../assets/Images/Character/Old/Boy.gif";
+        img.src ="../assets/Images/Character/Player/Boys/Boy 2/Gifs/Boy2 Idle R.gif";
+        img.setAttribute("style", "height: 366px; width: 183px; margin-left: 350px;");
+        
         break;
 
         case 3:
         sessionStorage.setItem("chosenCharacter", 3) //Boy 3
-        img.src ="../assets/Images/Character/Old/Brandle_Character.png";
+        img.src ="../assets/Images/Character/Player/Boys/Boy 3/Gifs/Boy3 Idle R.gif";
+        img.setAttribute("style", "height: 366px; width: 183px; margin-left: 350px;");
         break;
 
         case 4:
         sessionStorage.setItem("chosenCharacter", 4) //Girl 1
-        img.src ="../assets/Images/Character/Girl/Gifs/Girl_Idle_Left.gif";
+        img.src ="../assets/Images/Character/Player/Girls/Girl 1/Gifs/Girl1 Idle R.gif";
+        img.setAttribute("style","height: 358px; width: 223px; margin-left: 310px;");
         break;
 
         case 5:
         sessionStorage.setItem("chosenCharacter", 5) //Girl 2
-        img.src ="../assets/Images/Character/Old/Girl.gif";
+        img.src ="../assets/Images/Character/Player/Girls/Girl 2/Gifs/Girl2 Idle R.gif";
+        img.setAttribute("style", "height: 366px; width: 183px; margin-left: 340px;");
         break;
 
         case 6:
         sessionStorage.setItem("chosenCharacter", 6) //Girl 3
-        img.src ="../assets/Images/Character/Old/Strichmnaenchen1.png";
+        img.src ="../assets/Images/Character/Player/Girls/Girl 3/Gifs/Girl3 Idle R.gif";
+        img.setAttribute("style","height: 358px; width: 223px; margin-left: 310px;");
         break;
     }
 }
